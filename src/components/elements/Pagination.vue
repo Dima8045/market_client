2806cfbdd1c882d148613019043132034dce9200
Dmitry Derkach <dmitry.derkach@faceit-team.com>
2020-02-03 12:39:28 +0200

    Fixed products pagination

--- src/components/elements/Pagination.vue
+++ src/components/elements/Pagination.vue
@@ -7,7 +7,7 @@
                     <li v-if="getAllProducts().current_page - 2 > 0"><a style="cursor: pointer" @click.prevent="updateProductPage(getAllProducts().current_page - 2); fetchProducts(0)">{{ getAllProducts().current_page - 2 }}</a></li>
                     <li v-if="getAllProducts().current_page - 1 > 0"><a style="cursor: pointer" @click.prevent="updateProductPage(getAllProducts().current_page - 1); fetchProducts(0)">{{ getAllProducts().current_page - 1 }}</a></li>
                     <li class="active"><a>{{ getAllProducts().current_page }}</a></li>
-                    <li v-if="getAllProducts().current_page + 1 < getAllProducts().last_page"><a style="cursor: pointer" @click.prevent="updateProductPage(getAllProducts().current_page + 1); fetchProducts(0)">{{ getAllProducts().current_page + 1 }}</a></li>
+                    <li v-if="getAllProducts().current_page + 1 <= getAllProducts().last_page"><a style="cursor: pointer" @click.prevent="updateProductPage(getAllProducts().current_page + 1); fetchProducts(0)">{{ getAllProducts().current_page + 1 }}</a></li>
                     <li v-if="getAllProducts().current_page + 2 < getAllProducts().last_page"><a style="cursor: pointer" @click.prevent="updateProductPage(getAllProducts().current_page + 2); fetchProducts(0)">{{ getAllProducts().current_page + 2 }}</a></li>
                     <li v-if="getAllProducts().current_page + 1 < getAllProducts().last_page"><a style="cursor: pointer" @click.prevent="updateProductPage(getAllProducts().current_page + 1); fetchProducts(0)">&gt;</a></li>
                 </ul>
